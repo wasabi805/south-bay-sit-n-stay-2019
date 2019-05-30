aa836df208f32043c21e04fde6f5e08117b40b42
‘Tim <timothy.j.ocampo@gmail.com>
2019-05-30 15:10:09 -0700

    added @material-ui/core react component , @fontawesome/fontawesome-svg-core, @fontawesome/free-solid-svg-icons, and @fontawesome/react-fontawesome react component.

--- south-bay-sit-n-stay-2019/src/Components/page/page-container.js
+++ south-bay-sit-n-stay-2019/src/Components/page/page-container.js
@@ -1,10 +1,17 @@
 import React from "react";
 import PageStyle from "../../assets/style/components/page.scss";
+import Button from "@material-ui/core/Button";
 
-const PageContainer = () => {
+const PageContainer = props => {
   return (
     <div className={PageStyle.page}>
       <h1 className="header">This is the page container </h1>
+
+      <p>React Component : A Material Design button below</p>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+      {props.coffeeIcon}
     </div>
   );
 };
--- south-bay-sit-n-stay-2019/src/main.js
+++ south-bay-sit-n-stay-2019/src/main.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import PagePresenter from "./components/page/page-container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 class App extends Component {
   render() {
     let testThis = "This should render";
+    let coffeeIcon = <FontAwesomeIcon icon={faCoffee} />;
+
     return (
       <React.Fragment>
         <div className="hello">{testThis}</div>
-        <PagePresenter />
+        <PagePresenter coffeeIcon={coffeeIcon} />
       </React.Fragment>
     );
   }
