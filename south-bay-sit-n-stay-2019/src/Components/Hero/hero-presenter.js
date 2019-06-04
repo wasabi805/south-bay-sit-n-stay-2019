be4c83148526427e4546b5205e6d9de3de1ed545
‘Tim <timothy.j.ocampo@gmail.com>
2019-06-03 19:40:22 -0700

    Figured out how to use a media querry . See the notes in hero-presenter.js or https://stackoverflow.com/a/50780995/7857134 . Important part is to turn the theme into a function that returns createMuiTheme. Then read the stack overflow link in this comment regarding breakpoints.

--- south-bay-sit-n-stay-2019/src/Components/Hero/hero-presenter.js
+++ south-bay-sit-n-stay-2019/src/Components/Hero/hero-presenter.js
@@ -6,30 +6,42 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import teal from "@material-ui/core/colors/teal";
 
-const heroTheme = createMuiTheme({
-  palette: {
-    primary: teal,
-    text: {
-      primary: "#ffffff",
-      secondary: "#00000"
-    }
-  },
-
-  typography: {
-    fontFamily: "Roboto",
-
-    h1: {
-      color: "white",
-      fontSize: "3rem",
-      marginBottom: "2rem"
+//NOTE: Refactored theme into a function that returns createMuiTheme.
+//This is so I could use a media query on h1: the query is [theme.breakpoints.up().....]
+//see : https://stackoverflow.com/a/50780995/7857134
+const heroTheme = theme => {
+  return createMuiTheme({
+    palette: {
+      primary: teal,
+      text: {
+        primary: "#ffffff",
+        secondary: "#00000"
+      }
     },
 
-    h2: {
-      color: "white",
-      fontSize: "1.5rem"
+    typography: {
+      fontFamily: "Roboto",
+
+      h1: {
+        color: "white",
+        fontSize: "3rem",
+        marginBottom: "2rem",
+
+        [theme.breakpoints.up("md")]: {
+          color: "red"
+        }
+      },
+
+      h2: {
+        color: "white",
+        fontSize: "1.5rem"
+      }
     }
-  }
-});
+  });
+};
+
+// console.log([heroTheme.breakpoints.up('md')], 'BREAKPOINTS')
+// console.log([theme.breakpoints.up('md')], 'BREAKPOINTS')
 
 const HeroPresenter = props => {
   const HeroWrapper = styled(Container)({
@@ -96,13 +108,9 @@ const HeroPresenter = props => {
   return (
     <ThemeProvider theme={heroTheme}>
       <HeroWrapper className={props.heroClassName}>
-        <Typography variant="h1">
-          <h1> {props.heroHeader}</h1>
-        </Typography>
+        <Typography variant="h1">{props.heroHeader}</Typography>
 
-        <Typography variant="h2">
-          <h2>{props.heroSubHeader}</h2>
-        </Typography>
+        <Typography variant="h2">{props.heroSubHeader}</Typography>
 
         <HeroButtonContainer className={props.heroButtons}>
           <button>{props.heroButtonOne}</button>
