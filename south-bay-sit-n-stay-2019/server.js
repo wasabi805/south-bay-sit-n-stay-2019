2f519780afeca9f6c1963a4d7e45f30385f5be24
‘Tim <timothy.j.ocampo@gmail.com>
2019-07-22 12:51:23 -0700

    added PORT check for prod or dev env to server.js

--- south-bay-sit-n-stay-2019/server.js
+++ south-bay-sit-n-stay-2019/server.js
@@ -41,6 +41,14 @@ mongoose
 //USE ROUTES
 app.use("/api/booking/requests", bookingRequests);
 
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('dist'));
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+  })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
